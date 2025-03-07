74c58ebea60ac0095966fa2ffe9cb4324bb0b800
Sergio Alejandro Corcuera Mel <sergio.corcuera@munsurco.gob.pe>
2025-03-07 16:12:53 -0500

    07032025

--- resources/views/components/telefonos-emergencia.blade.php
+++ resources/views/components/telefonos-emergencia.blade.php
@@ -10,24 +10,44 @@
     </div>
     <div class="grid grid-cols-1 justify-items-center md:grid-cols-5 gap-4 text-center px-96 mb-12">
         <div>
-            <img class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
+            <img data-tooltip-target="logo113" class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
                  src="{{asset('assets/images/logo113.png')}}" alt="">
+            <div id="logo113" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xl font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
+                Si presenta sintomas del Covid-19 llamar al 113
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
+            <img data-tooltip-target="logoSerenazgo" class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
                  src="{{asset('assets/images/logoSerenazgo.png')}}" alt="">
+            <div id="logoSerenazgo" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xl font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
+                Serenazgo de Santiago de Surco - 4115555
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
+            <img data-tooltip-target="logoPNP" class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
                  src="{{asset('assets/images/logoPNP.png')}}" alt="">
+            <div id="logoPNP" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xl font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
+                Policia Nacional del Perú - 105
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
+            <img data-tooltip-target="logoSamu" class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
                  src="{{asset('assets/images/logoSamu.png')}}" alt="">
+            <div id="logoSamu" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xl font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
+                Servicio de Atención Medica de Urgencia - 106
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
+            <img data-tooltip-target="logoBomberos" class="h-auto max-w-full rounded-xl drop-shadow-md hover:drop-shadow-2xl p-4 cursor-pointer"
                  src="{{asset('assets/images/logoBomberos.png')}}" alt="">
+            <div id="logoBomberos" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-xl font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
+                Bomberos Voluntarios del Perú - 116
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
     </div>
 </div>
